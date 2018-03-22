9d49c9d28cea28c11364a650d4fe49b21c818943
Jukka Raivio <juraivio@gmail.com>
2018-03-22 10:25:11 +0200

    Update api.js

--- routes/orion/api.js
+++ routes/orion/api.js
@@ -1,6 +1,6 @@
 var request = require('request');
 var Promise = require('promise');
-var url="172.17.0.4";
+var url=location.host;
 
 var getAllOrionData=function getAllOrionData(){
       return new Promise(function (fulfill, reject){
